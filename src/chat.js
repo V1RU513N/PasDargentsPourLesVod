0cfe2e1d979c74153093fcd79f27742f1678df2a
Вирусиен <V12U513N@proton.me>
2022-09-01 01:53:53 +0200

    Update chat.js

--- src/chat.js
+++ src/chat.js
@@ -2,7 +2,7 @@ let chat = undefined;
 
 function addMessage(comment) {
     if (chat == undefined || !chat.length) {
-        chat = $("ul[class*='InjectLayout']");
+        chat = document.querySelector("ul[class*='InjectLayout']");
     }
 
     if (comment == undefined) {
@@ -40,8 +40,10 @@ function addMessage(comment) {
         }
     });
 
-    chat.append(`
-    <li class="InjectLayout-sc-588ddc-0 chat-element">
+    const li_element = document.createElement("li");
+    li_element.className = "InjectLayout-sc-588ddc-0 chat-element";
+
+    li_element.innerHTML = `
         <div class="Layout-sc-nxg1ff-0 custom-chat vod-message vod-message--timestamp" data-test-selector="message-layout">
             <div class="Layout-sc-nxg1ff-0 separator custom-chat-1 vod-message__header" data-test-selector="message-timestamp">
             
@@ -55,34 +57,28 @@ function addMessage(comment) {
                         <div class="ScAttachedTooltip-sc-v8mg6d-1 fpvEsi tw-tooltip" data-a-target="tw-tooltip-label" role="tooltip" id="${comment._id}" direction="top"></div>
                     </div>
                 </div>
-
             </div>
             <div class="Layout-sc-nxg1ff-0 chat-element">
                 <div class="Layout-sc-nxg1ff-0 custom-chat-3">
                     <div class="Layout-sc-nxg1ff-0 aleoz">
                         <span></span>
-
                         <a data-test-selector="comment-author-selector"
                             data-tt_content="tab_videos" data-tt_medium="video-message-author"
                             class="ScCoreLink-sc-udwpw5-0 username tw-link video-chat__message-author"
                             rel="noopener noreferrer" target="_blank" href="/${username}">
-
                             <span>
-                                 <span class="chat-author__display-name" data-a-target="chat-message-username" 
+                                <span class="chat-author__display-name" data-a-target="chat-message-username" 
                                     data-a-user="${username}" data-test-selector="message-username"
                                     style="color: ${user_color};">${username}</span>
                             </span>
                         </a>
-
                         <div class="Layout-sc-nxg1ff-0 inline video-chat__message" data-test-selector="comment-message-selector">
                             <span class="InjectLayout-sc-588ddc-0 separator">:</span>
                             <span class="">
                                 ${fragments_element}
                             </span>
                         </div>
-
                     </div>
-
                     <div class="Layout-sc-nxg1ff-0 kxZWcq video-chat__message-menu"
                         data-test-selector="menu-options-wrapper">
                         <div class="Layout-sc-nxg1ff-0 dbbxYG"></div>
@@ -90,16 +86,16 @@ function addMessage(comment) {
                 </div>
             </div>
         </div>
-    </li>
-    
-    `);
+    `;
 
-    if (!$("div[class*='unsynced']").length) {
-        chat[0].lastElementChild.scrollIntoView();
+    chat.appendChild(li_element);
+
+    if (!document.querySelector("div[class*='unsynced']")) {
+        chat.lastElementChild.scrollIntoView();
     }
 
-    if (chat[0].childElementCount >= 50) {
-        let nodes = chat[0].childNodes;
+    if (chat.childElementCount >= 50) {
+        let nodes = chat.childNodes;
 
         for (i = 0; i < 10; i++) {
             nodes[i].remove();
@@ -108,7 +104,7 @@ function addMessage(comment) {
 
 }
 
-function fetchChat(vod_id, offset, next) {
+async function fetchChat(vod_id, offset, next) {
     let url = "https://api.twitch.tv/v5/videos/" + vod_id;
 
     if (next == undefined) {
@@ -117,18 +113,13 @@ function fetchChat(vod_id, offset, next) {
         url += "/comments?cursor=" + next;
     }
 
-    console.log(url);
-
-    return $.ajax({
-        url: url,
+    return fetch(url, {
+        method: 'GET',
         headers: {
             "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko",
             "Accept": "application/vnd.twitchtv.v5+json"
-        },
-        type: 'GET',
-        dataType: 'html'
-    });
-
+        }
+    }).then(repsonse => repsonse.json());
 }
 
 function parseSeconds(sec_num) {
